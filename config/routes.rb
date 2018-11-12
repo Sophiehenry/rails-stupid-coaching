ba0c0b79605417ba6442a8d841dd6f738f04aff2
Sophie Henry <sophiesophiehenry@googlemail.com>
2018-11-12 14:23:46 +0800

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# customising routes. Generic syntax: verb 'path', to: 'controller#action', as: :route_name
+# http://localhost:3000/ask instead of /question/ask
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
